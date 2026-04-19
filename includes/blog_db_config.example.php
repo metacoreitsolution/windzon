934505424798ee57a6c5bcf8043caac2705763b9
brijesh dhorajiya <dhorajiyabrijesh607@outlook.com>
2026-04-19 14:24:35 +0530

    add the database connection

--- includes/blog_db_config.example.php
+++ includes/blog_db_config.example.php
@@ -8,16 +8,16 @@
 
 return [
     // Option 1: Full DSN string
-    'dsn'      => 'mysql:host=127.0.0.1;dbname=windzon_blog;charset=utf8mb4',
+    'dsn'      => 'mysql:host=127.0.0.1;dbname=u632365216_windzon_blogdb;charset=utf8mb4',
     
     // Option 2: Separate host and database (comment out dsn above if using this)
     // 'host'     => '127.0.0.1',
-    // 'database' => 'windzon_blog',
+    // 'database' => 'u632365216_windzon_blogdb',
     
     // Option 3: Unix socket (for MAMP on macOS, comment out dsn above if using this)
     // 'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
-    // 'database'    => 'windzon_blog',
+    // 'database'    => 'u632365216_windzon_blogdb',
     
-    'username' => 'root',
-    'password' => 'your_password_here',
+    'username' => 'u632365216_windzon_user',
+    'password' => 'Windzon$9419',
 ];
